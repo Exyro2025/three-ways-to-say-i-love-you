6ae65ca54bbb69e7f6ee232f489400175974901c
Exyro2025 <RLLEmail@proton.me>
2025-06-07 19:12:12 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function playSequence(voId, musicId) {
+  const vo = document.getElementById(voId);
+  const music = document.getElementById(musicId);
+
+  vo.pause();
+  music.pause();
+  vo.currentTime = 0;
+  music.currentTime = 0;
+
+  vo.play();
+  vo.onended = () => {
+    music.play();
+  };
+}
